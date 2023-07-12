a1e69531e220ea75067d3d6ba016d13de88c7dd6
Alex Kosova <whovian2504@yandex.ru>
2023-07-12 22:59:48 +0300

    пр-15

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,14 +6,14 @@ const cardRouter = require('./cards');
 const auth = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/users');
 
-routes.post('/signin', celebrate({
+routes.post('/api/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
   }),
 }), login);
 
-routes.post('/signup', celebrate({
+routes.post('/api/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
@@ -25,8 +25,8 @@ routes.post('/signup', celebrate({
 
 routes.use(auth);
 
-routes.use('/users', userRouter);
-routes.use('/cards', cardRouter);
+routes.use('/api/users', userRouter);
+routes.use('/api/cards', cardRouter);
 
 routes.use(() => {
   throw new NotFoundError('Страница не найдена');
